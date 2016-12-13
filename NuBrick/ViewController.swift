f9a71a1b264a64d61e4d1f0b39f68e31ec331f57
mwang <mwang@mwangs-Mac-mini.local>
2016-12-13 10:14:16 +0800

    Initial Commit

---
+++ NuBrick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NuBrick
+//
+//  Created by mwang on 13/12/2016.
+//  Copyright © 2016 nuvoton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
