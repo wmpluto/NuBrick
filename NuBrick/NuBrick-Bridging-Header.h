a69e869bbc883048a84123aa5fb35444e7550462
mwang <mwang@mwangs-Mac-mini.local>
2016-12-13 10:38:55 +0800

    Add Bridging Header
    bukdxb,xlzz

---
+++ NuBrick/NuBrick-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  NuBrick-Bridging-Header.h
+//  NuBrick
+//
+//  Created by mwang on 13/12/2016.
+//  Copyright © 2016 nuvoton. All rights reserved.
+//
+
+#ifndef NuBrick_Bridging_Header_h
+#define NuBrick_Bridging_Header_h
+
+#import <CoreBluetooth/CoreBluetooth.h>
+
+#endif /* NuBrick_Bridging_Header_h */
