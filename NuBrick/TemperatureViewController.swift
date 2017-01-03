a9b70f2372295106485c9120944709f55ec77f6b
pluto <mwang1@nuvoton.com>
2017-01-03 14:35:07 +0800

    Add Charts 3.0.1 to Project

--- NuBrick/TemperatureViewController.swift
+++ NuBrick/TemperatureViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import Charts
 
 class TemperatureViewController: UIViewController {
+    @IBOutlet weak var lineChartView: LineChartView!
+    
+    var lineBuffer:[Double] = Array(repeating: 0, count: 100)
+    var dataEntries: [ChartDataEntry] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //Set Default Line Chart View
+        for i in 0..<lineBuffer.count {
+            let dataEntry = ChartDataEntry(x: Double(i-3), y: Double(i*i-9))
+            dataEntries.append(dataEntry)
+        }
+        lineChartView.data = LineChartData(dataSet: LineChartDataSet(values: dataEntries, label: "default"))
     }
 
     override func didReceiveMemoryWarning() {
--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@ use_frameworks!
 
 target "NuBrick" do
     pod 'JGProgressHUD'
+    pod 'Charts', '~> 3.0.1'
 end
